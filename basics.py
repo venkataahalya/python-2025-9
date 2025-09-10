3de20830b79ea3cdf896b5b012faa537f775bdac
venkataahalya <venkataahalya@gmail.com>
2025-09-10 12:43:52 +0530

    10-9-2025

---
+++ basics.py
@@ -0,0 +1,33 @@
+# variables --int
+x=10
+y=20
+print(x+y)
+print(id(x))
+print(type(x))
+
+#string
+x="name"
+y='contry'
+print(x+" " +y)
+print(id(x)) # memory storage
+print(type(x)) #data type
+
+
+# list - can store multiple values to one variable
+# List closed with the Sruare brackets
+x=[10,20,30]
+print(x)
+
+# can concate string using +
+# add int using +
+# can't combine int and str together
+
+x=27
+y='ahalya'
+print("my name is"+ y)# can print string values
+print("my age"+x)# can't print test with int values
+
+
+
+
+
